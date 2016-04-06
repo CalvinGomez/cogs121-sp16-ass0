11bdecec912f54f1b5e3b690ae0ae04d44dcadee
Calvin Gomez <calvin.gomez1@gmail.com>
2016-04-06 15:34:40 -0700

    Removed unnecessary module cals

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var databaseModel = require('../models');
+// var databaseModel = require('../models');
 
 exports.view = function(req, res) {
 	mongoose.model('message').find(function (err, data) {
