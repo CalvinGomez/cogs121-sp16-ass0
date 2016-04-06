2d17a063bba9449f7cc589d6f5d5f0abd4dbabca
Calvin Gomez <calvin.gomez1@gmail.com>
2016-04-06 01:04:00 -0700

    Removed require moments dependency from routes/message.js

--- routes/message.js
+++ routes/message.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var models = require('../models');
-var moment = require('moment');
 
 exports.send = function(req,res) {
 	var email = req.body.email;
@@ -8,7 +7,6 @@ exports.send = function(req,res) {
 	var formData = new models.messageModel({
 	  	email: email,
 		content: content
-		// created: "04-24-2016"
 	});
 
 	formData.save(function(err, formData) {
